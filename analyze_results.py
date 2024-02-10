a6e8467d7d7b416f13295648099c82a2ca2b70e7
Clark Taylor <clark.n.taylor@gmail.com>
2024-02-10 14:42:40 +0000

    Modified analyze_results to only plot some of the results

--- analyze_results.py
+++ analyze_results.py
@@ -13,42 +13,47 @@ time_res = data['times']
 pos_RMSEs = data['pos_RMSEs']
 ang_RMSEs = data['ang_RMSEs']
 n_runs = data['times'].shape[2]
+est_to_plot = [0,1,2,5,6,7,8,9]
+in_to_plot = [0,1,3,5]
 
-fig1, axs = plt.subplots(len(in_opts), 1, figsize=(10, len(in_opts) * 5))
+fig1, axs = plt.subplots(len(in_to_plot), 1, figsize=(10, len(in_opts) * 5))
 for ii, ax in enumerate(axs):
-    data_to_plot = [pos_RMSEs[ii, jj, :] for jj in range(len(est_opts))]
+    data_to_plot = [pos_RMSEs[in_to_plot[ii], jj, :] for jj in est_to_plot]
     ax.violinplot(data_to_plot)
-    ax.set_xticks(np.arange(1, len(est_opts) + 1))
-    ax.set_xticklabels(est_opts)
-    ax.set_title(in_opts[ii])
+    ax.set_xticks(np.arange(1, len(est_to_plot) + 1))
+    ax.set_xticklabels(est_opts[est_to_plot])
+    ax.set_title(in_opts[in_to_plot[ii]])
     ax.set_xlabel('Estimation Options')
     ax.set_ylabel('Position RMSE (m)')
 
 plt.tight_layout()
+plt.savefig('pos_RMSEs.png')
 
-fig2, axs = plt.subplots(len(in_opts), 1, figsize=(10, len(in_opts) * 5))
+fig2, axs = plt.subplots(len(in_to_plot), 1, figsize=(10, len(in_opts) * 5))
 for ii, ax in enumerate(axs):
-    data_to_plot = [ang_RMSEs[ii, jj, :] for jj in range(len(est_opts))]
+    data_to_plot = [ang_RMSEs[in_to_plot[ii], jj, :] for jj in est_to_plot]
     ax.violinplot(np.array(data_to_plot).T * 180.0/np.pi)
-    ax.set_xticks(np.arange(1, len(est_opts) + 1))
-    ax.set_xticklabels(est_opts[:])
-    ax.set_title(in_opts[ii])
+    ax.set_xticks(np.arange(1, len(est_to_plot) + 1))
+    ax.set_xticklabels(est_opts[est_to_plot])
+    ax.set_title(in_opts[in_to_plot[ii]])
     ax.set_xlabel('Estimation Options')
     ax.set_ylabel('Angular RMSE (degrees)')
 
 plt.tight_layout()
+plt.savefig('ang_RMSEs.png')
 
-fig3, axs = plt.subplots(len(in_opts), 1, figsize=(10, len(in_opts) * 5))
+fig3, axs = plt.subplots(len(in_to_plot), 1, figsize=(10, len(in_opts) * 5))
 for ii, ax in enumerate(axs):
-    data_to_plot = [time_res[ii, jj, :] for jj in range(len(est_opts))]
+    data_to_plot = [time_res[in_to_plot[ii], jj, :] for jj in est_to_plot]
     ax.violinplot(data_to_plot)
-    ax.set_xticks(np.arange(1, len(est_opts) + 1))
-    ax.set_xticklabels(est_opts[:])
-    ax.set_title(in_opts[ii])
+    ax.set_xticks(np.arange(1, len(est_to_plot) + 1))
+    ax.set_xticklabels(est_opts[est_to_plot])
+    ax.set_title(in_opts[in_to_plot[ii]])
     ax.set_xlabel('Estimation Options')
     ax.set_ylabel('Time to Run (s)')
 
 plt.tight_layout()
+plt.savefig('time_res.png')
 
 plt.show()
 # %%
